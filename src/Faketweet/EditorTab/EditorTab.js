76375e66752892215efc4f82fc1821e6f30d16b3
Ankur Seth <iankurseth@gmail.com>
2022-10-28 15:43:58 +0530

    Added NumberInput for day, month, year

--- src/Faketweet/EditorTab/EditorTab.js
+++ src/Faketweet/EditorTab/EditorTab.js
@@ -1,4 +1,7 @@
-import {StatInput} from './Utils';
+import {
+	NumberInput,
+	StatInput
+} from './Utils';
 
 
 
@@ -22,9 +25,14 @@ export default function EditorTab ({data, setData}) {
 		<div className="min-h-screen bg-white rounded px-4 py-4">
 			<h2>EditorTab</h2>
 			<div className="lg:flex overflow-hidden py-4">
+				<NumberInput title="day" num={data.day} setNum={v => setX('day', v)} min={1} max={31} />
+				<NumberInput title="month" num={data.month} setNum={v => setX('month', v)} min={1} max={12} />
+				<NumberInput title="year" num={data.year} setNum={v => setX('year', v)} min={-1000} max={2200} />
+			</div>
+			<div className="lg:flex overflow-hidden py-4">
 				<StatInput title="retweets" stat={data.retweets} setStat={v => setX('retweets', v)} displayFunc={statDisplayFunc} />
-				<StatInput title="comments" stat={data.comments} setStat={v => setX('comments', v)} />
-				<StatInput title="likes" stat={data.likes} setStat={v => setX('likes', v)} />
+				<StatInput title="comments" stat={data.comments} setStat={v => setX('comments', v)} displayFunc={statDisplayFunc} />
+				<StatInput title="likes" stat={data.likes} setStat={v => setX('likes', v)} displayFunc={statDisplayFunc} />
 			</div>
 		</div>
 	);
--- src/Faketweet/EditorTab/Utils.js
+++ src/Faketweet/EditorTab/Utils.js
@@ -1,29 +1,73 @@
 
 
 
-export function StatInput ({
-	title="Stat", stat, setStat,
+export function NumberInput ({
+	title="Number", num, setNum,
 	min=false, max=false,
-	displayFunc = x => x,
-	validatorFunc = x => true
+	displayFunc = v => v,
+	validatorFunc = v => true
 }) {
 
 	const validate = v => {
+		if (isNaN(v)) return;
 		if (min !== false && min > v) return false;
 		if (max !== false && max < v) return false;
 		return validatorFunc(v) ? true : false;
 	};
 
 	const handleChange = event => {
-		if (isNaN(event.target.value)) return;
-		const v = parseInt(event.target.value);
+		const value = event.target.value.trim();
+		if (isNaN(value)) return;
+
+		const v = parseInt(value);
+		if (validate(v)) {
+			setNum(v);
+		}
+	};
+
+	return (
+		<div className="px-2 py-2">
+			<div>
+				<h2 className="px-2 py-2 text-center">
+					<span className="font-bold mr-2">{displayFunc(num)}</span>
+					<span className="text-sm">{title}</span>
+				</h2>
+			</div>
+			<div>
+				<input type='text' defaultValue={num} onChange={handleChange} className="w-full text-lg px-2 py-2 border-2 border-blue-500" />
+			</div>
+		</div>
+	);
+}
+
+
+
+export function StatInput ({
+	title="Stat", stat, setStat,
+	min=false, max=false,
+	displayFunc = v => v,
+	validatorFunc = v => true
+}) {
+
+	const validate = v => {
+		if (isNaN(v)) return;
+		if (min !== false && min > v) return false;
+		if (max !== false && max < v) return false;
+		return validatorFunc(v) ? true : false;
+	};
+
+	const handleChange = event => {
+		const value = event.target.value.trim();
+		if (isNaN(value)) return;
+
+		const v = parseInt(value);
 		if (validate(v)) {
 			setStat(v);
 		}
 	};
 
 	return (
-		<div className="px-2 py-2 grow">
+		<div className="px-2 py-2">
 			<div>
 				<h2 className="px-2 py-2 text-center">
 					<span className="font-bold mr-2">{displayFunc(stat)}</span>
