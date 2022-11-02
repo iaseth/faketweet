8f785c20ba10d3c443257689b83adc87acf55848
Ankur Seth <iankurseth@gmail.com>
2022-11-02 15:22:02 +0530

    Added the verified badge

--- src/Faketweet/TweetTab/TweetTab.js
+++ src/Faketweet/TweetTab/TweetTab.js
@@ -3,6 +3,16 @@ import { saveAs } from 'file-saver';
 
 
 
+function VerifiedBadge () {
+	return (
+		<svg viewBox="0 0 24 24" role="img" style={{fill: 'rgb(29, 155, 240)'}}>
+			<g>
+				<path d="M22.25 12c0-1.43-.88-2.67-2.19-3.34.46-1.39.2-2.9-.81-3.91s-2.52-1.27-3.91-.81c-.66-1.31-1.91-2.19-3.34-2.19s-2.67.88-3.33 2.19c-1.4-.46-2.91-.2-3.92.81s-1.26 2.52-.8 3.91c-1.31.67-2.2 1.91-2.2 3.34s.89 2.67 2.2 3.34c-.46 1.39-.21 2.9.8 3.91s2.52 1.26 3.91.81c.67 1.31 1.91 2.19 3.34 2.19s2.68-.88 3.34-2.19c1.39.45 2.9.2 3.91-.81s1.27-2.52.81-3.91c1.31-.67 2.19-1.91 2.19-3.34zm-11.71 4.2L6.8 12.46l1.41-1.42 2.26 2.26 4.8-5.23 1.47 1.36-6.2 6.77z"></path>
+			</g>
+		</svg>
+	);
+}
+
 function DownloadButton ({title, onClick}) {
 	return (
 		<div onClick={onClick} className="bg-blue-500 border-2 border-blue-600 text-sm font-bold text-white px-4 py-4 rounded shadow text-center cursor-pointer">{title}</div>
@@ -33,9 +43,11 @@ export default function TweetTab ({data, minuteDisplayFunc, monthDisplayFunc, st
 							<div className="h-full w-full bg-red-400"></div>
 						</div>
 						<div id="UserName" className="px-3 text-[15px]">
-							<div>
-								<span className="font-bold" style={{color: 'rgb(15, 20, 25)'}}>{data.name}</span>
-								<span></span>
+							<div className="flex">
+								<div className="font-bold" style={{color: 'rgb(15, 20, 25)'}}>{data.name}</div>
+								<div className="ml-1 w-5 h-5">
+									{data.verified && <VerifiedBadge />}
+								</div>
 							</div>
 							<div>
 								<span className="font-normal" style={{color: 'rgb(83, 100, 113)'}}>@{data.username}</span>
--- src/Faketweet/Utils/StatInput.js
+++ src/Faketweet/Utils/StatInput.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import {InputBox} from './TextInput';
 import {PlusMinusButton} from './common';
 
 
